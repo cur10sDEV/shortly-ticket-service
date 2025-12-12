a6c4889eb0b9a1a8b753ba7b68749e74513425f9
cur10sdev <specsx@protonmail.com>
2025-12-12 11:38:06 +0530

    feat(api): add cors

--- src/api/v1/app.ts
+++ src/api/v1/app.ts
@@ -4,12 +4,24 @@ import { Hono } from 'hono'
 import { requestId } from 'hono/request-id'
 import { secureHeaders } from 'hono/secure-headers'
 
+import { cors } from 'hono/cors'
 import { requestLogger } from './middlewares/request-logger.js'
 import { ticketsRouter } from './tickets/routes/tickets.route.js'
+import { parsedEnv } from './utils/env.js'
 
 export const app = new Hono().basePath('/api/v1')
 
 // middlewares
+app.use(
+  cors({
+    origin: [parsedEnv.SHORT_URL_API_SERVICE_BASE_URL],
+    // allowHeaders: ['Content-Type', 'Authorization'],
+    // allowMethods: ['POST', 'GET', 'OPTIONS'],
+    // exposeHeaders: ['Content-Length'],
+    maxAge: 600,
+    credentials: true,
+  }),
+)
 app.use(secureHeaders())
 app.use(requestId())
 app.use(requestLogger())
